79d13cdad370f1592f46d57c7e5b9e19296b1a88
Adrian Bowyer <adrian@reprappro.com>
2021-05-05 15:10:09 +0100

    First calibration results.

--- RepRapLtd/Software/CalibrationMapPython/CalibrationMap.py
+++ RepRapLtd/Software/CalibrationMapPython/CalibrationMap.py
@@ -2,6 +2,7 @@
 
 import serial
 import time
+import re
 
 arduinoPort = '/dev/ttyUSB0'
 reprapPort = '/dev/ttyACM0'
@@ -22,7 +23,9 @@ def MoveRepRap(x, y, z, f, port):
 def GetArduinoVoltages(usb):
  usb.write(str.encode('v\n'))
  data = usb.readline()
- return str(data.decode('ascii')).replace('\r', '').replace('\n', '')
+ data = str(data.decode('ascii'))
+ data = re.findall('\d+', data)
+ return data
 
 def OpenArduinoUSB():
  usb = serial.Serial(arduinoPort,115200,timeout=0.1)
@@ -34,21 +37,37 @@ def OpenRepRapUSB():
  #time.sleep(3) # Why so long???
  return usb
 
+def LogReadings():
+ while True:
+  print(GetArduinoVoltages(aUSB))
+  time.sleep(0.5)
 
 aUSB = OpenArduinoUSB()
-#while True:
-# print(GetArduinoVoltages(aUSB))
-# time.sleep(0.5)
+#LogReadings()
 
 rUSB = OpenRepRapUSB()
 MoveRepRap(0, 0, 0, 1000, rUSB)
-for z in range(11):
+
+zMax = 10
+xMax = 25
+file0 = open('hall-calibration0.csv', 'w')
+file1 = open('hall-calibration1.csv', 'w')
+
+for z in range(zMax + 1):
  MoveRepRap(0, 0, z, 1000, rUSB)
  time.sleep(2)
- for x in range(21):
+ for x in range(xMax + 1):
   MoveRepRap(x, 0, z, 1000, rUSB)
   time.sleep(0.5)
-  print(GetArduinoVoltages(aUSB))
+  data = GetArduinoVoltages(aUSB)
+  print(data[0], ' ', data[1])
+  file0.write(data[0])
+  file1.write(data[1])
+  if x < xMax:
+   file0.write(",")
+   file1.write(",")
+ file0.write("\n")
+ file1.write("\n")
 
 
 
