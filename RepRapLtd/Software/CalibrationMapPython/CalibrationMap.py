10b381313778e5e9a4a1a63717d39a95748c563b
Adrian Bowyer <adrian@reprappro.com>
2021-05-06 17:15:34 +0100

    First full calibration, with errors.

--- RepRapLtd/Software/CalibrationMapPython/CalibrationMap.py
+++ RepRapLtd/Software/CalibrationMapPython/CalibrationMap.py
@@ -3,6 +3,8 @@
 import serial
 import time
 import re
+import math as maths
+import sys
 
 arduinoPort = '/dev/ttyUSB0'
 reprapPort = '/dev/ttyACM0'
@@ -25,6 +27,7 @@ def GetArduinoVoltages(usb):
  data = usb.readline()
  data = str(data.decode('ascii'))
  data = re.findall('\d+', data)
+ data = (int(data[0]), int(data[1]))
  return data
 
 def OpenArduinoUSB():
@@ -42,6 +45,27 @@ def LogReadings():
   print(GetArduinoVoltages(aUSB))
   time.sleep(0.5)
 
+def V2Length(a, b):
+ d0 = a[0] - b[0]
+ d1 = a[1] - b[1]
+ return maths.sqrt(d0*d0 + d1*d1)
+
+def FindXZ(data, hallMatrix):
+ xz = (-1, -1)
+ closeValue = sys.float_info.max
+ for z in range(len(hallMatrix)):
+  v = hallMatrix[z]
+  for x in range(len(v)):
+   d = v[x]
+   dist = V2Length(d, data)
+   if dist < closeValue:
+    closeValue = dist
+    xz = (x, z)
+    if closeValue < 1:
+     return xz
+ return xz
+
+
 aUSB = OpenArduinoUSB()
 #LogReadings()
 
@@ -50,10 +74,13 @@ MoveRepRap(0, 0, 0, 1000, rUSB)
 
 zMax = 10
 xMax = 25
-file0 = open('hall-calibration0.csv', 'w')
-file1 = open('hall-calibration1.csv', 'w')
+file0 = open('../../Experiments/HallCalibration/hall-calibration0.csv', 'w')
+file1 = open('../../Experiments/HallCalibration/hall-calibration1.csv', 'w')
+
+hallMatrix = []
 
 for z in range(zMax + 1):
+ hallVector = []
  MoveRepRap(0, 0, z, 1000, rUSB)
  time.sleep(2)
  for x in range(xMax + 1):
@@ -61,13 +88,40 @@ for z in range(zMax + 1):
   time.sleep(0.5)
   data = GetArduinoVoltages(aUSB)
   print(data[0], ' ', data[1])
-  file0.write(data[0])
-  file1.write(data[1])
+  file0.write(str(data[0]))
+  file1.write(str(data[1]))
+  hallVector.append(data)
   if x < xMax:
    file0.write(",")
    file1.write(",")
+ hallMatrix.append(hallVector)
  file0.write("\n")
  file1.write("\n")
+
+file0.close()
+file1.close()
+
+errorFile = open('../../Experiments/HallCalibration/xzErrors.csv', 'w')
+errorMatrix = []
+
+for z in range(zMax + 1):
+ MoveRepRap(0, 0, z, 1000, rUSB)
+ time.sleep(2)
+ errorVector = []
+ for x in range(xMax + 1):
+  MoveRepRap(x, 0, z, 1000, rUSB)
+  time.sleep(0.5)
+  data = GetArduinoVoltages(aUSB)
+  xz = FindXZ(data, hallMatrix)
+  e = V2Length(xz, (x, z))
+  errorVector.append(e)
+  errorFile.write(str(e))
+  print(x, " ", z, " error: ", e)
+  if x < xMax:
+   errorFile.write(",")
+ errorFile.write("\n")
+ errorMatrix.append(errorVector)
+
 
 
 
